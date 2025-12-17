6c901b75fcda585dbda3684518f923d77382169b
Faisal Salama <fsalamava@gmail.com>
2025-12-16 22:34:53 -0500

    another python code

---
+++ test.py
@@ -0,0 +1,9 @@
+print("Programming in python is fun!")
+
+def add(a, b):
+    return a + b    
+print("Sum of 3 and 5 is:", add(3, 5))
+def multiply(a, b):
+    return a * b
+print("Product of 4 and 6 is:", multiply(4, 6))
+
